fdc5a90b675c611951a617a11918a9f793ec6dfd
amzva76-pixel <amzva76@gmail.com>
2026-02-08 15:50:16 +0500

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Wlecome to the world of programming!!!")
+
+#print a number
+print(4)
+
+#print next line after print
+print("Hello python \n")
+
+#print pass multiply arguments for printing
+print("Hello", 5)
+
+#print end arguments of print statements
+print("Welcome to",end= "+")
---
+++ activity2.py
@@ -0,0 +1,13 @@
+# Creating different varibles
+x=5
+y="Areesha"
+print(x)
+print(y)
+
+codingal="codingal"
+print(codingal)
+
+# Put the User Name
+name=input("enter your name: ")
+
+print("\n Hello" ,name, "\n Welcome to codingal")
